9e10fba4a523631384ec8d6d96a2f0c29171743c
simcha23 <simcarmahlangu@gmail.com>
2020-02-12 11:05:08 +0200

    "Initial commit"

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class Calculator {
+    public static void main(String[] args){
+        System.out.println("\n---------ADDITION----------\n");
+        System.out.println("The answer is:\t" + add(1,2,3,4,5));
+        System.out.println("The answer is:\t" + add(1,2));
+        System.out.println("The answer is:\t" + add(-1,-1));
+
+        System.out.println("\n---------MULTIPLICATIONS----------\n");
+
+        System.out.println("The answer is:\t" + multiply(1,3));
+        System.out.println("The answer is:\t" + multiply(-1,3));
+        System.out.println("The answer is:\t" + multiply(1,2,3,4,5));
+    }
+    public static int add(int ... num){
+
+//        int sum = 0;
+//        for(int value:num) {
+//            sum = sum + value;
+//        }
+        int sum = Arrays.stream(num).sum();
+
+        return sum;
+    }
+    public static int multiply(int ... num){
+
+        int multiply = 1;
+        for(int value : num) {
+            multiply *= value;
+        }
+
+        return multiply;
+    }
+}
+
