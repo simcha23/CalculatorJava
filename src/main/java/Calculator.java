c51823508df6d7c101db6fc917202eb3f0086968
simcha23 <simcarmahlangu@gmail.com>
2020-02-20 13:29:25 +0200

    add user iputs

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,39 +1,86 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Calculator {
+
+
     public static void main(String[] args){
+
+
+        Scanner scan = new Scanner(System.in);
+
+        ArrayList<Integer> integerList = new ArrayList<>();
+
+        for(int x = 0; x <= integerList.size(); x++){
+
+            System.out.println("Enter number " + (x + 1) + " or 'A/a' to terminate:");
+            String number = scan.nextLine();
+
+            if(!number.trim().equalsIgnoreCase("a")|| !number.trim().equalsIgnoreCase("A")){
+                if(numberOrNot(number)){
+
+                    integerList.add(Integer.parseInt(number));
+                }
+                else {
+                    System.out.println("Only integer number allowed");
+                    x = x - 1;
+                }
+
+            }
+            else{
+                break;
+            }
+
+
+        }
+
+        int[] array = new int[integerList.size()];
+        for(int z =0 ; z< integerList.size(); z++){
+            array[z] = integerList.get(z);
+            System.out.println(array[z]);
+        }
+
+
         System.out.println("\n---------ADDITION----------\n");
-        System.out.println("The answer is:\t" + add(1,2,3,4,5));
-        System.out.println("The answer is:\t" + add(1,2));
-        System.out.println("The answer is:\t" + add(-1,-1));
+
+        System.out.println("The answer is:\t" + add(array));
 
         System.out.println("\n---------MULTIPLICATIONS----------\n");
 
-        System.out.println("The answer is:\t" + multiply(1,3));
-        System.out.println("The answer is:\t" + multiply(-1,3));
-        System.out.println("The answer is:\t" + multiply(1,2,3,4,5));
-    }
-    public static int add(int ... num){
+        System.out.println("The answer is:\t" + multiply(array));
 
-//        int sum = 0;
-//        for(int value:num) {
-//            sum = sum + value;
-//        }
-        int sum = Arrays.stream(num).sum();
+    }
+    public static int add(int... num){
+
+        int sum = 0;
+        for(int value:num) {
+            sum = sum + value;
+        }
 
         return sum;
     }
-    public static int multiply(int ... num){
+    public static int multiply(int... num){
 
         int multiply = 1;
         for(int value : num) {
             multiply *= value;
         }
-//        for(int i = 0; i < num.length; i++) {
-//            multiply *= num[i];
-//        }
 
         return multiply;
+    }
+    public static boolean numberOrNot(String input)
+    {
+        try
+        {
+            Integer.parseInt(input);
+
+        }
+        catch(NumberFormatException ex)
+        {
+            return false;
+        }
+        return true;
     }
 }
 
